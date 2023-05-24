3b89fadae5e6ab6d3d2edcc4983ef16cb2751278
Alexander Vyzhymov <vizimov@gmail.com>
2023-05-24 21:46:56 +0300

    fixed-errors

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -36,7 +36,7 @@ const contactsSlice = createSlice({
     [deleteContact.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
-      const index = state.items.findIndex(item => item.id === action.payload);
+      const index = state.items.findIndex(({ id }) => id === action.payload.id);
       state.items.splice(index, 1);
     },
     [deleteContact.rejected]: handleRejected,
